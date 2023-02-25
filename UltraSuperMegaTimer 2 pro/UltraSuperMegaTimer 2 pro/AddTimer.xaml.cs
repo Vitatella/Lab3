de7b3ab755ac227db3386b81632e20244b979a42
Vitatella <116969929+Vitatella@users.noreply.github.com>
2023-02-25 23:17:48 +0700

    First commit

---
+++ UltraSuperMegaTimer 2 pro/UltraSuperMegaTimer 2 pro/AddTimer.xaml.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace UltraSuperMegaTimer_2_pro
+{
+    public partial class AddTimer : Window
+    {
+        private MainWindow _mainWindow;
+        public AddTimer(MainWindow mainWindow)
+        {
+            InitializeComponent();
+            _mainWindow = mainWindow;
+        } 
+
+        public AddTimer(MainWindow mainWindow, string name, DateTime dateTime)
+        {
+            InitializeComponent();
+            _mainWindow = mainWindow;
+            Name.Text = name;
+            Calendar.SelectedDate = dateTime;
+            Hours.Text = dateTime.Hour.ToString();
+            Minutes.Text = dateTime.Minute.ToString();
+            Seconds.Text = dateTime.Second.ToString();
+        }
+
+        private void AddNewTimer(object sender, RoutedEventArgs e)
+        {
+            if (Name.Text == null || Calendar.SelectedDate == null) return;
+
+            DateTime dateTime = new DateTime(Calendar.SelectedDate.Value.Year,
+                Calendar.SelectedDate.Value.Month,
+                Calendar.SelectedDate.Value.Day,
+                int.Parse(Hours.Text),
+                int.Parse(Minutes.Text),
+                int.Parse(Seconds.Text));
+
+            _mainWindow.AddNewTimer(Name.Text, dateTime);
+            Close();
+        }
+
+        private void Hours_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if ((int.TryParse(Hours.Text, out int hours) == false) || (hours < 0 || hours >= 24))
+            {
+                Hours.Text = 0.ToString();
+            }
+        }
+
+        private void Minutes_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if ((int.TryParse(Minutes.Text, out int minutes) == false) || (minutes < 0 || minutes >= 60))
+            {
+                Minutes.Text = 0.ToString();
+            }
+        }
+
+        private void Seconds_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if ((int.TryParse(Seconds.Text, out int seconds) == false) || (seconds < 0 || seconds >= 60))
+            {
+                Seconds.Text = 0.ToString();
+            }
+        }
+    }
+}
---
+++ UltraSuperMegaTimer 2 pro/UltraSuperMegaTimer 2 pro/MainWindow.xaml.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.PortableExecutable;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace UltraSuperMegaTimer_2_pro
+{
+    public partial class MainWindow : Window
+    {
+        private System.Windows.Threading.DispatcherTimer _timer;
+        private Dictionary<string, DateTime> _timers = new Dictionary<string, DateTime>();
+        private string _selectedTimerName;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            UpdateCurrentTime();
+
+
+
+            _timer = new System.Windows.Threading.DispatcherTimer();
+            _timer.Tick += new EventHandler(DispatcherTimer_Tick);
+            _timer.Interval = new TimeSpan(0, 0, 1);
+            _timer.Start();
+        }
+
+        private void DispatcherTimer_Tick(object sender, EventArgs e)
+        {
+            UpdateCurrentTime();
+        }
+        public void UpdateCurrentTime()
+        {
+            if (_selectedTimerName == null) return;
+
+            DateTime currentTime = DateTime.Now;
+            if (TimeFormat.SelectedIndex != -1)
+            {
+                TimeSpan timeSpan = _timers[_selectedTimerName] - new DateTime(
+                    currentTime.Year, currentTime.Month, currentTime.Day, currentTime.Hour, currentTime.Minute, currentTime.Second);
+
+                switch (TimeFormat.SelectedIndex)
+                {
+                    case 0:
+                        Time.Content = $"{timeSpan.Days}:{timeSpan.Hours}:{timeSpan.Minutes}:{timeSpan.Seconds}";
+                        break;
+                    case 1:
+                        Time.Content = $"{timeSpan.Hours + timeSpan.Days * 24}:{timeSpan.Minutes}:{timeSpan.Seconds}";
+                        break;
+                    case 2:
+                        Time.Content = $"{timeSpan.Minutes + (timeSpan.Hours + timeSpan.Days * 24) * 60}:{timeSpan.Seconds}";
+                        break;
+                    case 3:
+                        Time.Content = $"{timeSpan.Seconds + (timeSpan.Minutes + (timeSpan.Hours + timeSpan.Days * 24) * 60) * 60}";
+                        break;
+                }
+            }
+        }
+
+        private void TimeFormat_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            UpdateCurrentTime();
+        }
+
+        private void AddTimer_Click(object sender, RoutedEventArgs e)
+        {
+            AddTimer addWindow = new AddTimer(this);
+            addWindow.Show();
+        }
+
+        public void AddNewTimer(string name, DateTime date)
+        {
+            MenuItem menuItem = new MenuItem();
+            menuItem.Header = name;
+            menuItem.Click += Timer_Click;
+            if (_timers.ContainsKey(name) == false)
+            {
+                _timers.Add(name, date);
+                Timers.Items.Add(menuItem);
+            }
+            else
+            {
+                _timers[name] = date;
+            }
+            OpenTimer(name);
+        }
+
+        private void Timer_Click(object sender, RoutedEventArgs e)
+        {
+            string header = sender.ToString().Substring(
+                sender.ToString().IndexOf("Header:"),
+                sender.ToString().LastIndexOf(" ") - sender.ToString().IndexOf("Header:"));
+            header = header.Replace("Header:", string.Empty);
+            OpenTimer(header);
+        }
+
+        private void OpenTimer(string header)
+        {
+            _selectedTimerName = header;
+            UpdateCurrentTime();
+            Name.Content = _selectedTimerName;
+        }
+
+        private void Delete_Click(object sender, RoutedEventArgs e)
+        {
+            Timers.Items.Remove(_selectedTimerName); //Not worrking
+            _timers.Remove(_selectedTimerName);
+            _selectedTimerName = null;
+            Name.Content = string.Empty;
+            Time.Content = string.Empty;
+        }
+
+        private void Edit_Click(object sender, RoutedEventArgs e)
+        {
+            AddTimer addWindow = new AddTimer(this, _selectedTimerName, _timers[_selectedTimerName]);
+            addWindow.Show();
+        }
+    }
+}
